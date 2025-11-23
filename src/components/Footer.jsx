7baca960d68d3ca94f2575bad9ede2da549646c0
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2025-11-23 11:55:23 +0100

    colores

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="main-footer">
-      <p>© 2025 - Masterclass React</p>
+      <p>Anastasia Anaya, Lucía Macías, Enric Rubió, Beanne Manalo, Nora Pastoret, Paula Ruiz, Armand Puertos</p>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,12 @@ function Header() {
   return (
     <header className="main-header">
       <h1>Pel·lícules Ghibli</h1>
+      <div className="filter">
+      <img src="/assets/filter-icon.png" alt="Filter Icon" />
+      </div>
     </header>
+
+ 
   );
 }
 
