3ce1247b7a8ecdf2e98a4663128a8a72156421ad
Enric <enric.rubio.capdevila@estudiantat.upc.edu>
2025-11-25 11:34:46 +0100

    eliminació comentari

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App /> {/*En un futur fer que directament et porti a HomePage o Loading i eliminar App*/}
+    <App />
   </StrictMode>,
 )
