1a803d436f0ddc8930417a8b7fc15221844326f0
nora-pastoret <nora.pastoret@estudiantat.upc.edu>
2025-11-25 12:20:50 +0100

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
 import Header from './components/Header';
+import Menu from './components/Menu';
 import HomePage from './pages/HomePage';
+import InfoFilm from './pages/InfoFilm';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-     
+     <Header />
+
+     <Rouets>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/post/:id" element={<InfoFilm />}
+        <Route path="/" element={<Menu />} />
+     </Rouets>
+{/* FALTA MENU*/}
+      <Footer />
+
+      {/* aixo es el que hi havia abans:
       <main className="content">
         <HomePage />
-      </main>
-     
+      </main>*/}
+
+
     </div>
   )
 }
