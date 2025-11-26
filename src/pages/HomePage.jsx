6d4daa1fef74e41b9981667f5cb4edb817a4359a
Pusa <armand.puertos@estudiantat.upc.edu>
2025-11-26 12:24:11 +0100

    link routes mal

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Menu from '../components/Menu';
 import Footer from '../components/Footer';
 import './HomePage.css';
 
 const API_URL = 'https://ghibliapi.vercel.app/films';
+
+
 
 // --- OPCIONES DEL FILTRE ---
 const SORT_OPTIONS = {
@@ -83,14 +86,14 @@ function HomePage() {
       <div className="films-grid">
         {sortedFilms.map((film) => (
           film && (
-            <a
+            <Link
               key={film.id}
               href={`/film/${film.id}`}
               className="film-card"
               title={film.title}
             >
               <img src={film.image} alt={film.title} className="film-image" />
-            </a>
+            </Link>
           )
         ))}
       </div>
