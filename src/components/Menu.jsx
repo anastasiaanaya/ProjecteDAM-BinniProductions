4af1d921c08f6f73f04dfc2376dcf59cd9b50473
Enric <enric.rubio.capdevila@estudiantat.upc.edu>
2025-11-26 12:17:48 +0100

    avenços menú

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,10 +5,15 @@ function Menu() {
 
     <nav className="navigation-menu">
       <div className="navigation-pages">
+        <div className="home-page selected-page">
 
+        </div>
+        <div className="favourites-page">
+
+        </div>
       </div>
       <div className="search-bar">
-      
+        <span className="material-symbols-rounded search-icon">search</span>
       </div>
     </nav>
   );
