0dc1da40d1c86ec533263666f7a88bb62691a39f
Enric <enric.rubio.capdevila@estudiantat.upc.edu>
2025-12-01 18:23:04 +0100

    icona totoro

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,17 @@
 import './Menu.css';
+import totoroIconUrl from './totoro-icon.svg';
 
 function Menu() {
   return (
-
     <nav className="navigation-menu">
       <div className="navigation-pages">
         <div className="home-page selected-page">
-
+          <img src={totoroIconUrl} alt="Totoro" className="totoro-icon" />
         </div>
-        <div className="favourites-page">
-
-        </div>
+        
+        <div className="favourites-page"></div>
       </div>
+
       <div className="search-bar">
         <span className="material-symbols-rounded search-icon">search</span>
       </div>
