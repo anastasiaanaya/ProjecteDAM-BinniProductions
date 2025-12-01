f313258a9bf0141600bb94b71f4111947dcb2e22
anastasiaanaya <154979891+anastasiaanaya@users.noreply.github.com>
2025-12-01 17:36:04 +0100

    cambios mal

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import InfoFilm from './pages/InfoFilm';
-import Footer from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import FilmPage from './pages/FilmPage';
 
 function App() {
   return (
-    <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/film/:id" element={<InfoFilm />} />
-          </Routes>
-       
-    </BrowserRouter>
-    // <div className="App">
-     
-    //   <main className="content">
-    //     <InfoFilm />
-    //   </main>
-     
-    // </div>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/film/:id" element={<FilmPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
