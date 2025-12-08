94fc4d952feb424d0292ee0b03b4f27cc4b90ffe
Enric <enric.rubio.capdevila@estudiantat.upc.edu>
2025-12-08 17:49:02 +0100

    explicació codi menú

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,18 +5,23 @@ import heartIcon from './heart-icon.svg';
 import { useState } from 'react';
 
 function Menu() {
+  // Hook per saber a quina pàgina es troba l'usuari
   const location = useLocation();
   const path = location.pathname;
+
   const [searchExpanded, setSearchExpanded] = useState(false);
   const [searchValue, setSearchValue] = useState("");
 
   return (
     <nav className="navigation-menu">
+      {/* Si la barra de búsqueda està oberta s'afegeix la classe contracted, sinó res */}
       <div className={`navigation-pages${searchExpanded ? ' contracted' : ''}`}> 
-
+        
         {!searchExpanded && (
+          // Afegir la classe sliding-marker (icona seleccionada del menú) segons la pàgina on es troba l'usuari
           <div className={`sliding-marker${(path === '/' || path.startsWith('/film')) ? ' home' : (path.startsWith('/favourites') ? ' favourites' : ' home')}`} />
         )}
+
         {searchExpanded ? (
           (path === '/' || path.startsWith('/film')) ? (
             <div className="home-page contracted-page">
@@ -41,6 +46,7 @@ function Menu() {
 
       <div className={`search-bar${searchExpanded ? ' expanded' : ''}`}> 
         <span className="material-symbols-rounded search-icon" onClick={() => setSearchExpanded(true)}>search</span>
+        
         {searchExpanded && (
           <input
             type="text"
@@ -48,8 +54,8 @@ function Menu() {
             placeholder="Cerca per títol, director..."
             value={searchValue}
             onChange={e => setSearchValue(e.target.value)}
-            autoFocus
-            onBlur={() => setSearchExpanded(false)}
+            autoFocus // Quan s'obra la lupa ja està seleccionat el camp de text per escriure
+            onBlur={() => setSearchExpanded(false)} // Quan l'usuari clica a fora del buscador es tanca
           />
         )}
       </div>
