f7de3590da8fedd9fa4a68ce8dbb1ef63c716bca
beannemc <154535449+beannemanalo@users.noreply.github.com>
2025-12-08 13:07:08 +0100

    arreglar header h1 i filtres

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import './Header.css';
-import logoGhibli from '../../public/logo-ghibli2.svg';
+import logoGhibli from '../../public/logo-ghibli.svg';
 
 function Header({ sortOption, setSortOption, SORT_OPTIONS }) {
   return (
     <header className="header">    
       <section className="main-header">
 
-        <div className="logo-ghibli">
+        <div className="logo-ghibli">          
           <img src={logoGhibli} alt="Logo Ghibli" className="img-ghibli"/>
 
         </div>
         <div className="titol-ghibli">
-
+          <h1> kiki's library</h1>
+          
         </div>
         
         
