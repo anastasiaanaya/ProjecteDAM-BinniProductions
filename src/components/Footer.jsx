9b530d1f973ae90e6a88f6ddc8f32b3636252e3c
Enric <enric.rubio.capdevila@estudiantat.upc.edu>
2025-12-08 18:29:29 +0100

    canvi text footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="main-footer">
-      <p>Anastasia Anaya, Lucía Macías, Enric Rubió, Beanne Manalo, Nora Pastoret, Paula Ruiz, Armand Puertos</p>
+      <p>Aplicació creada amb React i utilitzant l'API Studio Ghibli API (v1.0.1)</p>
     </footer>
   );
 }
