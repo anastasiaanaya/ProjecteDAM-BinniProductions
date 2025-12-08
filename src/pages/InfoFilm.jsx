1a698e94b711945fc5cecdabf9736b4f8bf53819
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2025-12-08 19:52:14 +0100

    boton antiguo

--- src/pages/InfoFilm.jsx
+++ src/pages/InfoFilm.jsx
@@ -68,7 +68,7 @@ function InfoFilm() {
     <div className="film-detail">
       <div className="film-banner-wrap">
         <Link to="/" className="btn-back" aria-label="Volver">
-          <img src={back} alt="Volver" className="back-icon" />
+          <svg viewBox="0 0 24 24"><path d="M15 18l-6-6 6-6" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"/></svg>
         </Link>
 
         <button className="btn-fav" onClick={()=> toggleFavorite(film)}>
