00c858dcc7b34624ca744c3e3209fcae052063df
PaulaRuiz13 <paula.r.trivino@gmail.com>
2025-12-14 13:52:01 +0100

    ScrollToTop
    Añadido esto para que cuando cargue la pagina siempre se vaya al principio, que habia momentos en los que se cargaba mal la info

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import HomePage from './pages/HomePage';
 import InfoFilm from './pages/InfoFilm';
 import Header from './components/Header';
 import Menu from './components/Menu';
-
+import ScrollToTop from './components/ScrollToTop';
 import Favourites from './pages/Favourites';
 
 function App() {
   return (
 <div> 
   <Header />
+
+  <ScrollToTop />
   
   <Routes>
       <Route path="/" element={<HomePage />} />
